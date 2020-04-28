7c888f1752b8642089ec3712c36baaa605edc470
amcnichols <57888171+amcnichols@users.noreply.github.com>
2020-04-27 18:05:14 -0700

    fix data path

--- 16-D3/Instructions/StarterCode/assets/js/app.js
+++ 16-D3/Instructions/StarterCode/assets/js/app.js
@@ -20,7 +20,7 @@ var svg = d3.select("#scatter")
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-d3.csv("../starterCode/assets/data/data.csv").then(function(importData){
+d3.csv("../assets/data/data.csv").then(function(importData){
   
             
     importData.forEach(function (importData){
